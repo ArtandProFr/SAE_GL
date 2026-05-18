6f49568bc52333fbb85759a7488c4f0840e0d6fd
ArtandProFr <arthur.dworniczak@gmail.com>
2026-05-18 15:01:41 +0200

    Ajout de la classe MovingBallsButton.

---
+++ src/Enigmas/MovingBallsButton.java
@@ -0,0 +1,34 @@
+public class MovingBallsButton {
+    public Vec2 coord;
+    public double taille;
+    public String sens;
+    public boolean pressed; 
+    public boolean clicked; 
+
+    public MovingBallsButton(Vec2 coord, double taille, String sens){
+
+        /* Constructeur */
+
+        this.coord = coord;
+        this.taille = taille;
+        this.sens = sens; // UP, DOWN, LEFT, RIGHT
+        this.pressed = false;
+        this.clicked = false;
+    }
+
+    public void update(Vec2 mouseCoord, boolean leftClickPushed, boolean leftClickPressed){
+
+        /* Cette méthode met à jour le bouton en fonction des actions du joueur. */
+
+        pressed = false;
+        clicked = false;
+        if (leftClickPressed){
+            if (Math.abs(mouseCoord.x - coord.x) < taille/2 && Math.abs(mouseCoord.y - coord.y) < taille/2){
+                pressed = true;
+                if (leftClickPushed){
+                    clicked = true;
+                }
+            }
+        }
+    }
+}
